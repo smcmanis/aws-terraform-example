019acf7ce6a8f8820aeeb4b6a832f0c8c366fb40
smcmanis <simon.mcmanis@gmail.com>
2022-06-01 21:50:27 +1000

    Create VPC and subnets

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev-vpc"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.1.1.0/24","10.1.2.0/24"]
+  public_subnets  = ["10.1.3.0/24","10.1.4.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
