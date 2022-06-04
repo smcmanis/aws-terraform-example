6fe0be473769e19f8073257b70b15c1a72c5d9c7
smcmanis <simon.mcmanis@gmail.com>
2022-06-04 18:18:34 +1000

    Store resource output values in terraform state

---
+++ output.tf
@@ -0,0 +1,28 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = module.vpc.vpc_arn
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = module.vpc.default_security_group_id
+}
+
+output "public_subnets" {
+  value = module.vpc.private_subnets
+}
+
+#output "ec2_instance" {
+#  description = "The full output of the ec2 module"
+#  value       = module.ec2_instance
+# }
