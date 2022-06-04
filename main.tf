67a7b266ad07cabc8122e77c0c0e70adb8c2db02
smcmanis <simon.mcmanis@gmail.com>
2022-06-04 18:17:50 +1000

    Allow all outbound traffic

--- main.tf
+++ main.tf
@@ -12,29 +12,42 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  env = "dev"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev-vpc"
+  name = "${local.env}-vpc"
   cidr = "10.1.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.1.1.0/24","10.1.2.0/24"]
   public_subnets  = ["10.1.3.0/24","10.1.4.0/24"]
+  database_subnets = ["10.1.5.0/24","10.1.6.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = local.env
   }
 }
 
 resource "aws_security_group" "public-facing-sg" {
   name            = "public-facing-sg"
   description     = "Managed by terraform"
-  vpc_id          = module.vpc.vpc_id
+  vpc_id          = module.vpc.vpc_id 
+
+  egress {
+    description     = "Outbound traffic to any destination"
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "all"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Allow HTTP requests on port 80"
