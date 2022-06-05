9a6b74811224879541918bdb5f44d04810f122e6
smcmanis <simon.mcmanis@gmail.com>
2022-06-05 13:24:07 +1000

    Remove unused private subnets

--- main.tf
+++ main.tf
@@ -23,9 +23,8 @@ module "vpc" {
   cidr = "10.1.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.1.1.0/24","10.1.2.0/24"]
+  database_subnets = ["10.1.1.0/24","10.1.2.0/24"]
   public_subnets  = ["10.1.3.0/24","10.1.4.0/24"]
-  database_subnets = ["10.1.5.0/24","10.1.6.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
